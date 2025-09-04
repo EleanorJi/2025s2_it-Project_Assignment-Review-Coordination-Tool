cbdf6c4c700de01605723f97b48bac71e5e1dc42
EleanorJi <hanyuj2@student.unimelb.edu.au>
2025-09-04 19:04:54 +1000

    add dashboard redirect when login

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const db = require('../config/database');
 const { ROLES } = require('../config/constants');
+const redirectConfig = require('../config/redirect');
 
 exports.login = async (req, res) => {
   const { email, name, password } = req.body;
@@ -72,10 +73,14 @@ exports.login = async (req, res) => {
       last_login: user.last_login
     };
 
+    // 根据角色确定重定向路径
+    const redirectTo = redirectConfig[user.role] || redirectConfig.default;
+
     res.json({
       success: true,
       message: 'Login successful!',
-      user: userResponse
+      user: userResponse,
+      redirectTo: redirectTo  // 添加重定向路径
     });
 
   } catch (error) {
