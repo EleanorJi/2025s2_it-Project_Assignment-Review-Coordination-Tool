41022a7747d084b22c7ebaac519c94f8ba1e10c7
ruonannn <xiong.r@student.unimelb.edu.au>
2025-09-28 21:01:04 +1000

    add marker test account

--- database/seeds/initial_data.sql
+++ database/seeds/initial_data.sql
@@ -6,3 +6,12 @@ VALUES (
  'COORDINATOR',
  true
 );
+
+INSERT INTO app_user (name, email, password_hash, role, is_active)
+VALUES (
+ 'marker',
+ 'marker@grading.com',
+ 'marker123',
+ 'MARKER',
+ true
+);
