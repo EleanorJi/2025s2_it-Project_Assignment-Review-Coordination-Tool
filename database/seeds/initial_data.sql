045188d9521f8beebc5b65e862c7c221bbc5ee68
EleanorJi <hanyuj2@student.unimelb.edu.au>
2025-10-30 14:12:44 +1100

    fix hash encrypt package

--- database/seeds/initial_data.sql
+++ database/seeds/initial_data.sql
@@ -2,7 +2,7 @@ INSERT INTO app_user (name, email, password_hash, role, is_active)
 VALUES (
  'admin',
  'admin@grading.com',
- 'admin123',
+ '$2b$12$ai/bCmRjtkZgsi9nY/mnueawJEoBnKoHqWRtm.lu0nycUELBYYR9u',
  'COORDINATOR',
  true
 );
@@ -11,7 +11,7 @@ INSERT INTO app_user (name, email, password_hash, role, is_active)
 VALUES (
  'marker',
  'marker@grading.com',
- 'marker123',
+ '$2b$12$ai/bCmRjtkZgsi9nY/mnueawJEoBnKoHqWRtm.lu0nycUELBYYR9u',
  'MARKER',
  true
 );
