94d5dbd5c32d2fe859e591fddca72ee73da4b4a2
EleanorJi <hanyuj2@student.unimelb.edu.au>
2025-11-04 22:20:14 +1100

    update database file

--- database/IT SQL.sql
+++ database/IT SQL.sql
@@ -51,6 +51,7 @@ CREATE TABLE IF NOT EXISTS public.baseline_score
     criterion_id bigint NOT NULL,
     score numeric(5, 2) NOT NULL,
 	finalized boolean DEFAULT false,
+	deviation_percent NUMERIC(5, 2) DEFAULT 5.0,
     comment text COLLATE pg_catalog."default",
     CONSTRAINT baseline_score_pkey PRIMARY KEY (baseline_id),
     CONSTRAINT uq_baseline_assignment_criterion UNIQUE (assignment_id, criterion_id)
